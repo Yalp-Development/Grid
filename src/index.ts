b175c5e091f8a8ad4a8ab0376f59648907421c64
Cozmo <75683363+CozmoDev@users.noreply.github.com>
2024-12-22 19:03:42 +0200

    forgot to export enums

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export { default as GridService } from "./modules/grid/index.js"
 export { default as ThumbnailsRelay } from "./modules/thumbnail/index.js"
 
 export { default as ScriptExecution } from "./modules/grid/script.js"
+
+export { ThumbnailType, ImageFormat } from "./modules/thumbnail/enums.js"
